4f25b3f528518e599892f65fd54b2f64572c0c79
Louis Wang <louis@lero9.co.nz>
2016-03-15 11:14:59 +1300

    Lero9 WordpressPostTabs:create layout and block files for add/edit tabs in admin

---
+++ app/code/Lero9/WordpressPostTabs/Block/Adminhtml/Tab/Edit/Form.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2016 Lero9. All rights reserved.
+ */
+namespace Lero9\WordpressPostTabs\Block\Adminhtml\Tab\Edit;
+
+/**
+ * Adminhtml wordpress post tab edit form block
+ *
+ */
+class Form extends \Magento\Backend\Block\Widget\Form\Generic
+{
+    /**
+     * Prepare form
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        /** @var \Magento\Framework\Data\Form $form */
+        $form = $this->_formFactory->create(
+            ['data' => ['id' => 'edit_form', 'action' => $this->getData('action'), 'method' => 'post']]
+        );
+        $form->setUseContainer(true);
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
+}
