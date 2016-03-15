ff41cefd411b026dc040d90bbdb72fa509381f8f
Louis Wang <louis@lero9.co.nz>
2016-03-15 16:39:51 +1300

    Lero9 WordpressPostTabs: create admin controller for saving a tab

---
+++ app/code/Lero9/WordpressPostTabs/Controller/Adminhtml/Tab/Save.php
@@ -0,0 +1,73 @@
+<?php
+namespace Lero9\WordpressPostTabs\Controller\Adminhtml\Tab;
+
+use Magento\Backend\App\Action;
+use Magento\TestFramework\ErrorLog\Logger;
+
+class Save extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @param Action\Context $context
+     */
+    public function __construct(Action\Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Lero9_WordpressPostTabs::save');
+    }
+
+    /**
+     * Save action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($data) {
+            /** @var \Lero9\WordpressPostTabs\Model\Tab $model */
+            $model = $this->_objectManager->create('Lero9\WordpressPostTabs\Model\Tab');
+
+            $id = $this->getRequest()->getParam('tab_id');
+            if ($id) {
+                $model->load($id);
+            }
+
+            $model->setData($data);
+
+            $this->_eventManager->dispatch(
+                'wordpress_post_tab_prepare_save',
+                ['tab' => $model, 'request' => $this->getRequest()]
+            );
+
+            try {
+                $model->save();
+                $this->messageManager->addSuccess(__('You saved this Tab.'));
+                $this->_objectManager->get('Magento\Backend\Model\Session')->setFormData(false);
+                if ($this->getRequest()->getParam('back')) {
+                    return $resultRedirect->setPath('*/*/edit', ['post_id' => $model->getId(), '_current' => true]);
+                }
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\RuntimeException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addException($e, __('Something went wrong while saving the tab.'));
+            }
+
+            $this->_getSession()->setFormData($data);
+            return $resultRedirect->setPath('*/*/edit', ['tab_id' => $this->getRequest()->getParam('tab_id')]);
+        }
+        return $resultRedirect->setPath('*/*/');
+    }
+}
